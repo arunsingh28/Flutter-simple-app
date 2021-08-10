67714e9823ff68302397a5918eab874b1e33f56b
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-10 23:23:09 +0530

    Version change

--- lib/Screen/Welcome/welcome_screen.dart
+++ lib/Screen/Welcome/welcome_screen.dart
@@ -2,7 +2,7 @@ import 'package:auth_ui/components/body.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({Key? key}) : super(key: key);
+  const WelcomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,15 +1,20 @@
 import 'package:auth_ui/components/background.dart';
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_svg/svg.dart';
 
 class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
+  const Body({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Background(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text("asdfadf")],
+        children: [
+          Text("Simple App", style: TextStyle(fontWeight: FontWeight.bold)),
+          SvgPicture.asset("assets/icons/chat.svg")
+        ],
       ),
     );
   }

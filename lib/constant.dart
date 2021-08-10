8cf87b9aca7b1e84f1aa340600e339242cbcdd0d
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-10 22:23:13 +0530

    ADD: constants

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFF6F35A5);
-const kPrimaryLightColor = Color(0xFFF1E6FF);
+const PrimaryColor = Color(0xFF6F35A5);
+const PrimaryLightColor = Color(0xFFF1E6FF);

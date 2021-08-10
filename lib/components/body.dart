150ec332a451254ec2d79a065fb944a19db19e21
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-10 22:39:26 +0530

    Postioning

---
+++ lib/components/body.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // total height & width of device
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/main_top.png",
+                width: size.width * .3,
+              ))
+        ],
+      ),
+    );
+  }
+}
